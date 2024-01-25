3d27cb7ceb44ab28e3d2cbdd1c6bb9e00c231ea1
Alyssa <altu.merto.up@phinmaed.com>
2024-01-25 19:48:21 +0800

    SignIn xml

--- app/src/main/java/com/example/flamespace/MainActivity.kt
+++ app/src/main/java/com/example/flamespace/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.example.flamespace
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.example.flamespace.R.id.stud
+
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val buttonClick = findViewById<Button>(stud)
+        buttonClick.setOnClickListener {
+            val intent = Intent(this, SignIn::class.java)
+            startActivity(intent)
+        }
+
+        val btn = findViewById<Button>(stud)
+        btn.setOnClickListener {
+            val intent = Intent(this, SignIn::class.java)
+            startActivity(intent)
+        }
     }
 }
